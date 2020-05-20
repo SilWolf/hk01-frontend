666acc5654cde932d83079d726286122838049f5
SilWolf <silwolf1121@gmail.com>
2020-05-20 22:29:27 +0800

    update App.js for basic UI structure

---
+++ src/components/SuggestionItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SuggestionItem() {
+  return (
+    <div></div>
+  );
+}
+
+export default SuggestionItem;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Searchbar from "./Searchbar";
+import SuggestionItem from "./SuggestionItem";
+import ListItem from "./ListItem";
+
+export {
+  Searchbar,
+  SuggestionItem,
+  ListItem
+};
